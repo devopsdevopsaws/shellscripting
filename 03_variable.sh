bf54940be716fde6706dcc55dc7410e8469c3f9c
Abilash-devops <abilashdevopsaws@gmail.com>
2024-02-24 12:30:22 +0530

    commit

--- 03_variable.sh
+++ 03_variable.sh
@@ -4,5 +4,23 @@ N2=$2
 echo "*************************************"
 ech0 "Adding the above two numbers"
 N=$((N1+N2))
-echo "Result is :$N"
+echo "Addition Result is :$N"
+echo "*************************************"
+
+echo "*************************************"
+ech0 "subratcting the above two numbers"
+N=$((N1-N2))
+echo "subtrction Result is :$N"
+echo "*************************************"
+
+echo "*************************************"
+ech0 "Multiplying the above two numbers"
+N=$((N1*N2))
+echo "Multiplication Result is :$N"
+echo "*************************************"
+
+echo "*************************************"
+ech0 "Dividing the above two numbers"
+N=$((N1/N2))
+echo "Division Result is :$N"
 echo "*************************************"
