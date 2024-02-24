c76085c8d0bd20b75538387acdba80f799fa6936
Abilash-devops <abilashdevopsaws@gmail.com>
2024-02-24 12:28:55 +0530

    commit

---
+++ 03_variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+N1=$1
+N2=$2
+echo "*************************************"
+ech0 "Adding the above two numbers"
+N=$((N1+N2))
+echo "Result is :$N"
+echo "*************************************"
