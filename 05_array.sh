18b7b08dfa69b31a4043d741424e1c66bc118ba5
Abilash-devops <abilashdevopsaws@gmail.com>
2024-02-24 12:52:58 +0530

    commit

--- 05_array.sh
+++ 05_array.sh
@@ -3,3 +3,6 @@ echo "Will understand array concepts"
 name=( "padma" "Srikanth" "kakarla" )
 echo "The name of the first person is: ${name[0]}"
 echo "The name of the persons are : ${name[@]}"
+echo "The name of the persons are : ${name[#]}"
+echo "The name of the persons are : ${name[*]}"
+echo "The name of the scripts are : $0"
