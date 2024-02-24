52cb6ccbced7a5a212fac9362885b80c20cfa630
Abilash-devops <abilashdevopsaws@gmail.com>
2024-02-24 15:55:36 +0530

    commit

---
+++ 09_deleteoldlog.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+date=$(date +%F-%H-%M-%S)
+script_name=$0
+script_path=/home/ec2-user/shellscripting/logs
+APP_LOG=/home/ec2-user/shellscripting/applogs
+LOGFILE=$script_path/$script_name-$date.log
+
+f=$(find $APP_LOG -name *.txt -mtime +14 )
+
+echo "script started executing at $DATE" &>>$LOGFILE
